1e31f21044db66a4dc3025de60e6e6f19e3fe6de
Ryan Hileman <lunixbochs@gmail.com>
2017-04-28 15:18:58 -0700

    tweak ida loading

--- ida_frontend.py
+++ ida_frontend.py
@@ -6,13 +6,35 @@ from client import Client
 from config import config
 
 get_fhash = retrieve_input_file_sha256
-fhash = get_fhash()
+fhash = None
+client = Client(**config)
+
+def onmsg(key, data):
+    if key != fhash or key != retrieve_input_file_sha256():
+        print 'hash mismatch, dropping command'
+        return
+    print 'msg', data
+
+def on_load():
+    global fhash
+    if fhash:
+        client.leave(fhash)
+    fhash = get_fhash()
+    client.join(fhash, onmsg)
 
 class IDPHooks(IDP_Hooks):
     def renamed(self, ea, new_name, local_name):
         if isLoaded(ea):
             client.publish(get_fhash(), {'cmd': 'rename', 'addr': ea, 'text': new_name})
         return IDP_Hooks.renamed(self, ea, new_name, local_name)
+
+    def newfile(self, fname):
+        on_load()
+        return IDP_Hooks.newfile(self, fname)
+
+    def oldfile(self, fname):
+        on_load()
+        return IDP_Hooks.oldfile(self, fname)
 
 class IDBHooks(IDB_Hooks):
     def cmt_changed(self, ea, repeatable):
@@ -28,16 +50,6 @@ class IDBHooks(IDB_Hooks):
     def area_cmt_changed(self, cb, a, cmt, repeatable):
         client.publish(get_fhash(), {'cmd': 'area_comment', 'range': [a.startEA, a.endEA], 'text': cmt})
         return IDB_Hooks.area_cmt_changed(self, cb, a, cmt, repeatable)
-
-def onmsg(key, data):
-    if key != fhash or key != retrieve_input_file_sha256():
-        print 'hash mismatch, dropping command'
-        return
-    print 'msg', data
-
-print 'revsync connecting with hash %s' % fhash
-client = Client(**config)
-client.join(fhash, onmsg)
 
 hook1 = IDPHooks()
 hook2 = IDBHooks()
