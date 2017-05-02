d75d8b374b7376ef4fb53b4f7e5177824b8ced08
dropkick <kwmcmullen@gmail.com>
2017-05-02 15:35:27 -0400

    binja read-only symbol rename support

--- binja_frontend.py
+++ binja_frontend.py
@@ -16,7 +16,10 @@ def get_ea(addr):
     return addr + bv.start
 
 def get_func_by_addr(addr):
-    return bv.get_basic_blocks_at(addr)[0].function
+    bb = bv.get_basic_blocks_at(addr)
+    if len(bb) > 0:
+        return bb[0].function
+    return None
 
 def onmsg(key, data, replay=False):
     if key != fhash:
@@ -34,6 +37,23 @@ def onmsg(key, data, replay=False):
         log_info('revsync: <%s> %s %s %s' % (user, cmd, data['range'], data['text']))
     elif cmd == 'rename':
         log_info('revsync: <%s> %s %#x %s' % (user, cmd, data['addr'], data['text']))
+        addr = get_ea(int(data['addr']))
+        sym = bv.get_symbol_at(addr)
+        if sym is not None:
+            # symbol already exists for this address
+            if sym.auto == True:
+                bv.undefine_auto_symbol(sym)
+            else:
+                bv.undefine_user_symbol(sym)
+        # is it a function?
+        func = get_func_by_addr(addr)
+        if func is not None:
+            # function 
+            sym = types.Symbol(SymbolType.FunctionSymbol, addr, data['text'])
+        else:
+            # data
+            sym = types.Symbol(SymbolType.DataSymbol, addr, data['text'])
+        bv.define_user_symbol(sym)
     elif cmd == 'join':
         log_info('revsync: <%s> joined' % (user))
     else:
