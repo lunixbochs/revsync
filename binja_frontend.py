6c0b4a039bcb67a6565974cdd9b3518d65e751ba
dropkick <kwmcmullen@gmail.com>
2017-05-01 21:35:49 -0400

    binja read-only comments support

--- binja_frontend.py
+++ binja_frontend.py
@@ -4,17 +4,21 @@ import hashlib
 from client import Client 
 from config import config
 
-log_info('revsync loaded!!!!')
-
 def get_fhash(fname):
     with open(fname, 'rb') as f:
         return hashlib.sha256(f.read()).hexdigest().upper()
 
 fhash = None
+bv = None
 client = Client(**config)
 
+def get_ea(addr):
+    return addr + bv.start
+
+def get_func_by_addr(addr):
+    return bv.get_basic_blocks_at(addr)[0].function
+
 def onmsg(key, data, replay=False):
-    log_info('revsync: callback fired')
     if key != fhash:
         log_info('revsync: hash mismatch, dropping command')
         return
@@ -22,6 +26,8 @@ def onmsg(key, data, replay=False):
     cmd, user = data['cmd'], data['user']
     if cmd == 'comment':
         log_info('revsync: <%s> %s %#x %s' % (user, cmd, data['addr'], data['text']))
+        addr = get_ea(int(data['addr']))
+        get_func_by_addr(addr).set_comment(addr, data['text'])
     elif cmd == 'extra_comment':
         log_info('revsync: <%s> %s %#x %s' % (user, cmd, data['addr'], data['text']))
     elif cmd == 'area_comment':
@@ -33,10 +39,12 @@ def onmsg(key, data, replay=False):
     else:
         log_info('revsync: unknown cmd %s' % data)
 
-def load(bv):
+def load(view):
     global fhash
+    global bv
     if fhash:
         client.leave(fhash)
+    bv = view
     fhash = get_fhash(bv.file.filename)
     log_info('revsync: connecting with %s' % fhash)
     client.join(fhash, onmsg)
