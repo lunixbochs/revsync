228be21c56a3ab102ecf93d0339de92dc12d7770
Grazfather <grazfather@gmail.com>
2018-06-17 13:44:35 -0700

    binja: Show popup warning no config

--- __init__.py
+++ __init__.py
@@ -2,12 +2,16 @@ import sys
 import os
 sys.path.append(os.path.dirname(__file__))
 
-import config
-config = config.config
 good = False
 
 try:
     import binaryninja
+    import binaryninja.interaction
+    try:
+        import config
+    except ImportError:
+        binaryninja.interaction.show_message_box(title="Revsync error", text="Revsync: You must setup myconfig.py!")
+        raise
     import binja_frontend
     good = True
 except ImportError:
