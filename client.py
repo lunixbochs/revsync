b0705fa22ebacd88ee00da25a5f9cf45b20588ea
Matt Thomas <mattypiper@gmail.com>
2018-08-10 13:48:23 -0700

    Add connect timeout
    Avoid blocking IDA startup by adding a timeout for the redis
    server connection

--- client.py
+++ client.py
@@ -48,7 +48,8 @@ def remove_ttl(a):
 
 class Client:
     def __init__(self, host, port, nick, password=None):
-        self.r = redis.StrictRedis(host=host, port=port, password=password)
+        self.r = redis.StrictRedis(host=host, port=port, password=password, socket_timeout=5, socket_connect_timeout=3)
+        self.r.info()
         self.nick = nick_filter.sub('_', nick)
         self.ps = {}
         self.nolock = threading.Lock()
